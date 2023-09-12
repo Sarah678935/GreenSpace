ab540531cfb65dc529527908973b955adfd7b525
Sarah <sarah.salam3679@gmail.com>
2023-09-12 18:43:49 +0300

    Initial commit

---
+++ php/home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="ico" href="../imgs/logo/logo-icon.ico">
+    <link rel="stylesheet" href="../css/home.css">
+    <title>GreenSpace</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+            <li><img class="logo-img" src="../imgs/logo/logo.png" alt="logo"></li>
+            <li class="brand-name">GreenSpace</li>
+                <li><a href="#" class="home">Home</a></li>
+                <li><a href="#" class="about">About</a></li>
+                <li><a href="#" class="shop">Shop</a></li>
+                <li><a href="#" class="contact">Contact</a></li>
+                <li><a href="#" class="blog">Blog</a></li>
+                <li><a href="#" class="login">Login</a></li>
+                <li><a href="#" class="cart"><img class="cart-logo" src="../imgs/logo/ShoppingCart.svg" alt=""></a></li>
+
+            </ul>
+
+
+        </nav>
+    </header>
+    <div class="blur"></div>
+    <h2>Welcom To GreenSpace</h2>
+</body>
+
+</html>
